520ec7e9729f5c7f86b6fec19d7e7a1267cc5a69
kurashov <x.kurashov@gmail.com>
2020-11-02 19:32:28 +0300

    algorithm for solving quadratic equation, change output style - fixes

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,6 +3,8 @@ from math import *
 
 def quadratic_equation(a, b, c):
     if a == 0:
+        if c == 0:
+            return [-1] if b == 0 else []
         return [-b / c]
     d = b * b - 4 * a * c
     if d < 0:
@@ -11,7 +13,7 @@ def quadratic_equation(a, b, c):
         return [-b / (2 * a)]
     x1 = (-b - sqrt(d)) / (2 * a)
     x2 = (-b + sqrt(d)) / (2 * a)
-    return [x1, x2]
+    return [x1, x2] if x1 <= x2 else [x2, x1]
 
 
 inputData = input()
@@ -19,6 +21,6 @@ a, b, c = [int(x) for x in inputData.split(',')]
 
 res = quadratic_equation(a, b, c)
 if len(res) == 0:
-    print(-1)
+    print(0)
 else:
     [print(x, end=' ') for x in res]
